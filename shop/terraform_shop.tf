c820b65fdde377b5dff0d3696e43e8a808d0c445
Frederique Retsema <fretsema@fretsema.nl>
2020-04-10 18:59:08 +0000

    Technical Debt: Added comments to the shop tf, also deleted environmentvariable for keys for the process lambda

--- shop/terraform_shop.tf
+++ shop/terraform_shop.tf
@@ -47,9 +47,33 @@ data "aws_route53_zone" "my_zone" {
 
 ##################################################################################
 # RESOURCES
+#
+# Order: from client to DynamoDB
+# - Connection to certificate
+# - Route53
+# - API Gateway
+# - Lambda function accept
+# - SNS topic to_decrypt
+# - Lambda function decrypt
+# - SNS topic to_process
+# - Lambda function process
 ##################################################################################
 
+#
+# Connection to certificate
+#
+# Is needed to be sure that the certificate is validated
+#
+
+resource "aws_acm_certificate_validation" "mycertificate_validation" {
+    certificate_arn = data.aws_acm_certificate.domain_certificate.arn
+}
+
+#
 # Route 53
+# 
+# DNS-entry for <user_prefix>.<your domainname>, will point to the gateway domain
+#
 
 resource "aws_route53_record" "my_shop_dns_record" {
     zone_id = data.aws_route53_zone.my_zone.zone_id
@@ -63,27 +87,53 @@ resource "aws_route53_record" "my_shop_dns_record" {
     }
 }
 
-# Connection to certificate
-
-resource "aws_acm_certificate_validation" "mycertificate_validation" {
-    certificate_arn = data.aws_acm_certificate.domain_certificate.arn
-}
-
+#
 # API Gateway
-
-resource "aws_api_gateway_domain_name" "API_Gateway_domain_name" {
-  domain_name              = "${lower(var.user_prefix)}.${var.domainname}"
-  regional_certificate_arn = aws_acm_certificate_validation.mycertificate_validation.certificate_arn
-  security_policy = "TLS_1_2"
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
+#
+# These objects are defined in the order in which they are used from Route53, via the moment the POST 
+# request is done to the API gateway, to the Lambda function it is calling:
+# - aws_api_gateway_base_path_mapping : will define what stage and what method (if any, not used here)
+#                                       is used when the DNS record is used. You can have multiple 
+#                                       mappings (f.e. for multiple stages, or including the 
+#                                       "resource" path - or not) for one domain.
+#                                       This mapping is only used when you use a route 53 DNS entry 
+#                                       to go to the API gateway)
+# - aws_api_gateway_domain_name       : links the name of the domain (and the certificate) to the 
+#                                       API gateway that it is using
+#                                       (only used when you use a route 53 DNS entry to go to the 
+#                                        API gateway)
+# - aws_api_gateway_deployment        : is the version of the API. We only use prod in our
+#                                       example. This is also the object that contains the DNS
+#                                       for the API, f.e.
+#                                       https://4rgbh0ivlf.execute-api.eu-west-1.amazonaws.com/prod )
+# - aws_api_gateway_rest_api          : the API gateway itself. Is also used when you use the 
+#                                       DNS-name that the API gateway provides you (f.e.
+# - aws_api_gateway_resource          : this is the "/shop" part of the URL. 
+# - aws_api_gateway_method            : this is the way the "/shop" part is addressed. In our example,
+#                                       this is POST (could also have been GET)
+# - aws_api_gateway_integration       : is the connection between the API gateway and the Lambda 
+#                                       function that is behind it. 
+#
 
 resource "aws_api_gateway_base_path_mapping" "map_shop_prod_to_api_gateway_domain" {
   api_id = aws_api_gateway_rest_api.API_Gateway.id
   stage_name = aws_api_gateway_deployment.deployment_prod.stage_name
   domain_name = aws_api_gateway_domain_name.API_Gateway_domain_name.domain_name
+}
+
+resource "aws_api_gateway_domain_name" "API_Gateway_domain_name" {
+  domain_name              = "${lower(var.user_prefix)}.${var.domainname}"
+  regional_certificate_arn = aws_acm_certificate_validation.mycertificate_validation.certificate_arn
+  security_policy          = "TLS_1_2"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_deployment" "deployment_prod" {
+  depends_on  = [aws_api_gateway_integration.integration]
+  rest_api_id = aws_api_gateway_rest_api.API_Gateway.id
+  stage_name  = "prod"
 }
 
 resource "aws_api_gateway_rest_api" "API_Gateway" {
@@ -117,13 +167,19 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = aws_lambda_function.accept.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "deployment_prod" {
-  depends_on = [aws_api_gateway_integration.integration]
-  rest_api_id = aws_api_gateway_rest_api.API_Gateway.id
-  stage_name = "prod"
-}
-
-# Lambda
+#
+# Lambda accept
+#
+# These objects are defined in the order in which they are used from the call 
+# from the API gateway to the SNS topic:
+# - aws_lambda_permissions: connection between the API gateway (which needs 
+#                           permission to call this specific Lambda function)
+#                           and the Lambda function accept.
+# - aws_lambda_function:    the actual Lambda function. The use of a zip file
+#                           is mandatory in Terraform.
+#                           We use environment variables to pass the AWS Resource Name (ARN) of the
+#                           SNS topic to the Lambda code.
+# 
 resource "aws_lambda_permission" "lambda_accept_permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -131,14 +187,12 @@ resource "aws_lambda_permission" "lambda_accept_permission" {
   principal     = "apigateway.amazonaws.com"
 }
 
-# Lambda accept
-
 resource "aws_lambda_function" "accept" {
     function_name = "${var.user_prefix}_accept"
-    filename = "./lambdas/accept/accept.zip"
-    role = data.aws_iam_role.lambda_accept_role.arn
-    handler = "accept.lambda_handler"
-    runtime = "python3.8"
+    filename      = "./lambdas/accept/accept.zip"
+    role          = data.aws_iam_role.lambda_accept_role.arn
+    handler       = "accept.lambda_handler"
+    runtime       = "python3.8"
     environment {
         variables = {
             to_decrypt_topic_arn = aws_sns_topic.to_decrypt.arn
@@ -147,7 +201,12 @@ resource "aws_lambda_function" "accept" {
 }
 
 # SNS topic to_decrypt
-# - For a description of the resend policies for Lambda, see https://docs.aws.amazon.com/sns/latest/dg/sns-message-delivery-retries.html
+#
+# These objects are defined in the order in which they are used from the call 
+# from Lambda function accept to the delivery to the Lambda function 
+# decrypt:
+# - aws_sns_topic:              topic of the SNS messaging system
+# - aws_sns_topic_subscription: subscribers to the topic
 
 resource "aws_sns_topic" "to_decrypt" {
   name = "${var.user_prefix}_to_decrypt"
@@ -159,7 +218,18 @@ resource "aws_sns_topic_subscription" "decrypt_to_lambda" {
   endpoint  = aws_lambda_function.decrypt.arn
 }
 
-# Lambda permission
+# Lambda decrypt
+#
+# These objects are defined in the order in which they are used from the call 
+# from the SNS topic to the lambda function:
+# - aws_lambda_permissions: connection between the SNS topic to_decrypt (which needs
+#                           permission to call this specific Lambda function)
+#                           and the Lambda function decrypt.
+# - aws_lambda_function:    the actual Lambda function. The use of a zip file
+#                           is mandatory in Terraform.
+#                           We use environment variables to pass the AWS Resource Name (ARN) of the
+#                           SNS topic and the key prefix to the Lambda code.
+
 resource "aws_lambda_permission" "lambda_decrypt_permission" {
   statement_id  = "AllowExecutionFromSNS"
   action        = "lambda:InvokeFunction"
@@ -168,14 +238,12 @@ resource "aws_lambda_permission" "lambda_decrypt_permission" {
   source_arn    = aws_sns_topic.to_decrypt.arn
 }
 
-# Lambda process
-
 resource "aws_lambda_function" "decrypt" {
     function_name = "${var.user_prefix}_decrypt"
-    filename = "./lambdas/decrypt/decrypt.zip"
-    role = data.aws_iam_role.lambda_decrypt_role.arn
-    handler = "decrypt.lambda_handler"
-    runtime = "python3.8"
+    filename      = "./lambdas/decrypt/decrypt.zip"
+    role          = data.aws_iam_role.lambda_decrypt_role.arn
+    handler       = "decrypt.lambda_handler"
+    runtime       = "python3.8"
     environment {
         variables = {
             key_prefix           = var.key_prefix,
@@ -185,7 +253,12 @@ resource "aws_lambda_function" "decrypt" {
 }
 
 # SNS topic to_process
-# - For a description of the resend policies for Lambda, see https://docs.aws.amazon.com/sns/latest/dg/sns-message-delivery-retries.html
+#
+# These objects are defined in the order in which they are used from the call 
+# from Lambda function decrypt to the delivery to the Lambda function 
+# process:
+# - aws_sns_topic:              topic of the SNS messaging system
+# - aws_sns_topic_subscription: subscribers to the topic
 
 resource "aws_sns_topic" "to_process" {
   name = "${var.user_prefix}_to_process"
@@ -197,7 +270,16 @@ resource "aws_sns_topic_subscription" "process_to_lambda" {
   endpoint  = aws_lambda_function.process.arn
 }
 
-# Lambda permission
+# Lambda process
+#
+# These objects are defined in the order in which they are used from the call 
+# from the SNS topic to the lambda function:
+# - aws_lambda_permissions: connection between the SNS topic to_process (which needs
+#                           permission to call this specific Lambda function)
+#                           and the Lambda function process.
+# - aws_lambda_function:    the actual Lambda function. The use of a zip file
+#                           is mandatory in Terraform.
+
 resource "aws_lambda_permission" "lambda_process_permission" {
   statement_id  = "AllowExecutionFromSNS"
   action        = "lambda:InvokeFunction"
@@ -206,26 +288,22 @@ resource "aws_lambda_permission" "lambda_process_permission" {
   source_arn    = aws_sns_topic.to_process.arn
 }
 
-# Lambda process
-
 resource "aws_lambda_function" "process" {
     function_name = "${var.user_prefix}_process"
     filename = "./lambdas/process/process.zip"
     role = data.aws_iam_role.lambda_process_role.arn
     handler = "process.lambda_handler"
     runtime = "python3.8"
-    environment {
-        variables = {
-            key_prefix = var.key_prefix
-        }
-    }
 }
 
 ##################################################################################
 # OUTPUT
+#
+# The output is needed for people who don't have (or don't want to use) a
+# domain in Route53
 ##################################################################################
 
 output "invoke_url" {
-  value = aws_api_gateway_deployment.deployment_prod.invoke_url
+  value = "${aws_api_gateway_deployment.deployment_prod.invoke_url}/${aws_api_gateway_resource.API_Gateway_resource_shop.path_part}"
 }
 
