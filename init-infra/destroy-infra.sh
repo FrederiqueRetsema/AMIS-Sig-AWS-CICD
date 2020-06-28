6c5115baa48dcd8338c5632371902b79a2bf11a8
Frederique Retsema <fretsema@fretsema.nl>
2020-06-28 16:53:40 +0000

    Deleted comment about shop

--- init-infra/destroy-infra.sh
+++ init-infra/destroy-infra.sh
@@ -2,10 +2,6 @@
 # ----------------
 # Will use terraform to destroy the infrastructure
 #
-# WARNING:
-# --------
-# Destroy the shop FIRST, before you destroy the infrastructure!
-#
 # If you use the vagrant environment, use /home/vagrant/destroy-all.sh
 # (which will call this script in the right way)
 
